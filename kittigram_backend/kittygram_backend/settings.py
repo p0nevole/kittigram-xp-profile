dd1faf47a8c87d0d1e5299f8f939f80b39df0c57
Miron Shestopalov <mironshestopalov@MacBook-Pro-Miron.local>
2026-06-17 14:43:30 +0300

    добавил сваггер редок

--- kittigram_backend/kittygram_backend/settings.py
+++ kittigram_backend/kittygram_backend/settings.py
@@ -26,6 +26,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'djoser',
     'cats.apps.CatsConfig',
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
--- kittigram_backend/kittygram_backend/urls.py
+++ kittigram_backend/kittygram_backend/urls.py
@@ -6,6 +6,11 @@ from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
 
+from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
 from cats.views import AchievementViewSet, CatViewSet
 
 
@@ -13,12 +18,27 @@ router = routers.DefaultRouter()
 router.register(r'cats', CatViewSet)
 router.register(r'achievements', AchievementViewSet)
 
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Kittygram API",
+        default_version='v1',
+        description="Документация API проекта Kittygram",
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+
     path('api/', include(router.urls)),
-    path('api/', include('djoser.urls')),  # Работа с пользователями
-    path('api/', include('djoser.urls.authtoken')),  # Работа с токенами
+    path('api/', include('djoser.urls')),
+    path('api/', include('djoser.urls.authtoken')),
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    
